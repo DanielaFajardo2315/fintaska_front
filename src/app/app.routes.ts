2f2929fcc4c92d9bfa9daef071950554355cf3c9
lapao-db <lp.daza59@gmail.com>
2025-10-04 21:10:10 -0500

    Crea componentes de finanzas, planner, profile-edit y pages admin, finances, register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Finances } from './pages/finances/finances';
+import { Register } from './pages/register/register';
+import { Admin } from './pages/admin/admin';
+
+
+export const routes: Routes = [
+    {path: 'finances', component:Finances, title: 'Finanzas'},
+    {path: 'register', component:Register, title: 'Registro'},
+    {path: 'admin', component:Admin, title: 'Administrador'}
+];
---
+++ src/app/components/finance-last-moves/finance-last-moves.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finance-last-moves',
+  imports: [],
+  templateUrl: './finance-last-moves.html',
+  styleUrl: './finance-last-moves.css'
+})
+export class FinanceLastMoves {
+
+}
