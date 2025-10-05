7e492d2c22f975cfb616884e1969c63efcfa5b2e
lapao-db <lp.daza59@gmail.com>
2025-10-04 21:10:10 -0500

    Crea componentes de finanzas, planner, profile-edit y pages admin, finances, register

---
+++ src/app/components/finance-card/finance-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finance-card',
+  imports: [],
+  templateUrl: './finance-card.html',
+  styleUrl: './finance-card.css'
+})
+export class FinanceCard {
+
+}
