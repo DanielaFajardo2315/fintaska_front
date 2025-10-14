58491b801c6e075071620083f9669b9304333c83
lapao-db <lp.daza59@gmail.com>
2025-10-14 18:58:04 -0500

    Funcionalidad: agrega sercives e interface de Finance y Task

---
+++ src/app/services/finance.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Finance } from '../interfaces/finance.interface';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinanceService {
+  //1. Inyectar: metodos, proveedores, ... dependencias o directivas de Angular. 
+  private _httpClient = inject(HttpClient); //variable = inject(dependencia)
+  
+  //2. Definir la ruta de acceso al back
+  private apiUrl = environment.appUrl; //de la variable de entorno llamamos la url de la app
+
+  //3. Métodos para hacer las peticiones
+
+  //peticion POST - crear movimiento
+  postFinance(financeToCreate: Finance){
+    return this._httpClient.post(`${this.apiUrl}/finances`,financeToCreate);
+  }
+
+  //peticion GET - obtener todos los movimientos
+  getFinances(){
+    return this._httpClient.get(`${this.apiUrl}/finances`);
+  }
+
+  //peticion PUT
+  putFinance(financeToUpdate :Finance, idFinanceUpdate: string){
+    return this._httpClient.put(`${this.apiUrl}/finances/${idFinanceUpdate}`,financeToUpdate)
+  }
+
+    
+
+  //peticion DELETE
+  deleteFinance(idFinanceDelete:string){
+    return this._httpClient.delete(`${this.apiUrl}/finances/${idFinanceDelete}`)
+  }
+
+
+  
+}
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Task } from '../interfaces/task.interface';
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+   //1. Inyectar: metodos, proveedores, ... dependencias o directivas de Angular. 
+  private _httpClient = inject(HttpClient); //variable = inject(dependencia)
+  
+  //2. Definir la ruta de acceso al back
+  private apiUrl = environment.appUrl; //de la variable de entorno llamamos la url de la app
+
+  //3. Métodos para hacer las peticiones
+
+  //peticion POST - crear tarea
+  postTask(taskToCreate: Task){
+    return this._httpClient.post(`${this.apiUrl}/tasks`,taskToCreate);
+      }
+    
+      //peticion GET - obtener todas las tareas
+      getTasks(){
+        return this._httpClient.get(`${this.apiUrl}/tasks`);
+      }
+    
+      //peticion PUT - actualizar tarea
+      putTask(taskToUpdate :Task, idTaskUpdate: string){
+        return this._httpClient.put(`${this.apiUrl}/tasks/${idTaskUpdate}`,taskToUpdate)
+      }
+    
+        
+    
+      //peticion DELETE
+      deleteTask(idTaskDelete:string){
+        return this._httpClient.delete(`${this.apiUrl}/tasks/${idTaskDelete}`)
+      }
+    
+
+}
